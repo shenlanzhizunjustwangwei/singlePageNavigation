5ba5b2201f784ea650789c0f94fbc9f752160e2e
Jamal Hasuev <metafra@mail.ru>
2017-10-16 04:59:21 +0300

    Adding all files (need some fixes)

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+var header = $(".js-header");
+var headerH = header.innerHeight();
+
+// $( window ).resize(function() {
+// 	headerH = header.innerHeight();
+// });
+
+function headerFixing() {
+	$("body").css({
+		paddingTop : headerH
+	});
+
+	header.css({
+		position : "fixed",
+		top : 0,
+		left : 0,
+		right : 0
+	});
+}
+
+// Just fixing our header
+headerFixing();
+
+/* menu toggle button */
+function toggleMainMenu(x) {
+	x.classList.toggle("change");
+	menuShowHide();
+}
+
+/* показ/скрытие меню */
+function menuShowHide(){
+
+	var menu = $(".js-menu");
+
+	menu.slideToggle(200, function(){
+		if (menu.is(":not(:visible)")) {
+			menu.removeAttr("style");
+		}
+	});
+
+}
+
+$(".js-menu").singlePageNavigation({
+	exceptClass : "menu__element_except",
+	disabledClass : "menu__element_no-clickable",
+	current : "active",
+	duration : 500,
+	delay : 0,
+	ease : "swing",
+	offset : headerH,
+	changeAddress : false,
+	currentClickable : true,
+	beforeScrolling : function(){
+		// console.log("start")
+	},
+	finishScrolling : function(){
+		// console.log("finish")
+	}
+});
---
+++ js/onePageNav.js
@@ -0,0 +1,121 @@
+/*
+ * jQuery Single Page Navigation Plugin
+ * http://github.com/davinskiy/singlePageNavigation
+ *
+ * @version 1.0.0
+ *
+ */
+
+;(function($){
+	var name = "singlePageNavigation";
+	$.fn[name] = function(options){
+		
+		var container = $(this);
+		if (!container.data(name)){	
+
+			var f = function(){
+
+				// Settings
+				options.exceptClass = options.exceptClass || "";
+				options.disabledClass = options.disabledClass || "";
+				options.current = options.current || "current";
+				options.duration = options.duration || 700;
+				options.delay = options.delay || 0;
+				options.ease = options.ease || "swing";
+				options.offset = options.offset || 0;
+				options.changeAddress = options.changeAddress || false;
+				options.currentClickable = options.currentClickable || true;
+				options.beforeScrolling = options.beforeScrolling || function(){};
+				options.finishScrolling = (options.finishScrolling)? options.finishScrolling : null;
+
+				var menu_items_li = $(container).children("li");
+				var menu_items_a = $(container).find("a");
+				var scrolled = false;
+
+				// Scroll processing
+				$(document).on("scroll", function() {
+
+					if (scrolled) return;
+
+					setTimeout(function(){
+						var documentScroll = $(this).scrollTop();
+						var doc_view = (documentScroll + $( window ).height());
+
+						for(var i = 0; i < menu_items_a.length; i++){
+							if(menu_items_li.eq(i).hasClass(options.exceptClass)){
+								continue;
+							}
+							var item_pos = $(menu_items_a.eq(i).attr("href")).offset().top;
+
+							if (doc_view > item_pos) {
+								menu_items_li.removeClass("active");
+								menu_items_li.eq(i).addClass("active");
+							}
+
+						}
+						scrolled = false;
+
+					}, 50);
+					scrolled = true;
+				});
+
+				// Click processing
+				menu_items_a.on("click", function(e){
+					
+					if( !$(this).parent().hasClass(options.exceptClass) ){
+						e.preventDefault();
+
+						// Disable menu item for click
+						if ($(this).parent().hasClass(options.disabledClass)) {
+							return false;
+						}
+
+						// Clickable current menu item
+						if (options.currentClickable === false) {
+							if ($(this).parent().hasClass(options.current)) {
+								return false;
+							}
+						}
+
+						// Function that execute before scrolling
+						if(options.beforeScrolling)
+							options.beforeScrolling();
+
+						// If we choice to change address of our browser, we do it
+						if (options.changeAddress) {
+							try {
+								history.pushState(null, null, $(this).attr("href"));
+							} catch(e) {}
+							location.hash = '#' + $(this).attr("href");
+						}
+
+						// Position to scroll with offset
+						var scrollTo = $($(this).attr("href")).offset().top - options.offset;
+						var completeCalled = false;
+
+						setTimeout(function(){
+
+							// Scrolling to the position that we need
+							$('html, body').animate({
+								scrollTop: scrollTo
+							}, {
+								duration : options.duration,
+								easing : options.ease,
+								complete : function(){
+									if(!completeCalled){
+										completeCalled = true;
+										options.finishScrolling();
+									}
+								}
+							});
+						}, options.delay);
+					}
+				});
+			}
+			container.data( name, f() );
+		}
+		else {
+			return container.data(name);
+		}
+	}
+})(jQuery);
